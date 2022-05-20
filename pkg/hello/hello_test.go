0898480388587443a1afeabcc05186eed078fced
Vincent Demeester <vincent@sbr.pm>
2022-05-20 17:32:59 +0200

    Hello CDCon 2022
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkg/hello/hello_test.go
+++ pkg/hello/hello_test.go
@@ -3,7 +3,7 @@ package hello
 import "testing"
 
 func TestSayHello(t *testing.T) {
-	expected := "Hello World!"
+	expected := "Hello cdcon 2022!"
 	actual := SayHello()
 	if actual != expected {
 		t.Errorf("expected %s, got %s", expected, actual)
