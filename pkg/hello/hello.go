bd7250cfee7c533a340c0a22724542645d7b5ffa
Vincent Demeester <vincent@sbr.pm>
2022-05-20 18:06:15 +0200

    Back to world
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkg/hello/hello.go
+++ pkg/hello/hello.go
@@ -1,5 +1,5 @@
 package hello
 
 func SayHello() string {
-	return "Hello cdcon 2022!"
+	return "Hello World!"
 }
--- pkg/hello/hello_test.go
+++ pkg/hello/hello_test.go
@@ -3,7 +3,7 @@ package hello
 import "testing"
 
 func TestSayHello(t *testing.T) {
-	expected := "Hello cdcon 2022!"
+	expected := "Hello World!"
 	actual := SayHello()
 	if actual != expected {
 		t.Errorf("expected %s, got %s", expected, actual)
